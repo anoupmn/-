6b1650e4cbf940e1dd5d4f0a5194eb1c32fc26a2
曾志鹏 <2557296177@qq.com>
2026-04-06 18:08:17 +0800

    chore(miniprogram): set subscription template id for reminder flow

--- miniprogram/config/notification.js
+++ miniprogram/config/notification.js
@@ -1,6 +1,6 @@
 // 在微信公众平台 -> 订阅消息中创建模板后，把模板 ID 填到这里。
 // 示例: 'm7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-const SUBSCRIBE_TEMPLATE_IDS = ['PHASE5_TEMPLATE_PENDING'];
+const SUBSCRIBE_TEMPLATE_IDS = ['KbyhLGWWfElaNLUNkztaFZ4ctfzOHaDe0zt6w46Vob8'];
 
 module.exports = {
   SUBSCRIBE_TEMPLATE_IDS
--- miniprogram/config/notification.ts
+++ miniprogram/config/notification.ts
@@ -1,3 +1,3 @@
 // 在微信公众平台 -> 订阅消息中创建模板后，把模板 ID 填到这里。
 // 示例: 'm7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-export const SUBSCRIBE_TEMPLATE_IDS = ['PHASE5_TEMPLATE_PENDING'];
+export const SUBSCRIBE_TEMPLATE_IDS = ['KbyhLGWWfElaNLUNkztaFZ4ctfzOHaDe0zt6w46Vob8'];
